b1e272e980dc2f7a24ee7c3e570886c15e5a3c25
Alberto Garcia <mmhgarcia@gmail.com>
2025-11-08 23:33:24 -0400

    img ssize

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ fetch('data.json')
     data.forEach(item => {
       if (item.stock === 0) return;
       const card = document.createElement('div');
+      card.style.width = '300px';
+      card.style.height = '300px';
+      card.style.overflow = 'hidden';
       card.className = 'card';
       card.innerHTML = `
-        <img src="images/${item.imagename}" alt="${item.sabor}">
+        <img src="img/${item.imagename}" alt="${item.sabor}">
         <h3>${item.sabor}</h3>
         <p>Código: ${item.codigo}</p>
         <div class="stock">Stock: ${item.stock}</div>
